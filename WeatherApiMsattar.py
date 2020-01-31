b48872f932a3e2ac01d533cc6fe9de159f18a65c
Mahmoud Abd Elsattar <57304901+mahmoud-abdelsattar@users.noreply.github.com>
2020-01-31 02:28:35 +0200

    Add files via upload

---
+++ WeatherApiMsattar.py
@@ -0,0 +1,47 @@
+"""
+WEATHER API Using City Names!!!
+"""
+import random
+from flask import jsonify, request, Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '<h1>Weather API by Mahmoud Abd Elsattar !</h1>'
+
+
+# Key is city name
+# Value is temperature
+cities_temperatures = {'Al Ain': 33, 'Amsterdam': 19, 'Toronto': 21,
+                       'Roma': 22, 'Alexandria': 29, 'Athens': 20,
+                       'Cairo': 30, 'Berlin': 18, 'Paris': 19,
+                       'Madrid': 18, 'Barcelona': 20, 'Istanbul': 24}
+
+
+@app.route('/weather/<city>')
+def get_temperature(city):
+    """get weather temperature by city
+    """
+    res = ''
+    try:
+
+        temp_response = cities_temperatures.get(city)
+        if temp_response is not None:
+            # return the response parsed in a json object
+            # key is temperature for clients to use it to get the city temperature
+            return jsonify({"temperature": temp_response})
+        else:
+            return jsonify({"temperature": "not is not existed in our database Sorry For good luck next time!!!! "
+                                           "\U0001F603."})
+    except:
+        return jsonify("Something went wrong!!!")
+
+    # return the response parsed in a json object
+    # return jsonify(response)
+
+
+if __name__ == "__main__":
+    # app.run(debug=True)
+    app.run()
